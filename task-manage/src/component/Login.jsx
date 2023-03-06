dbe059fc44677c7aa4f325a2d9c44f99bf57bef2
Brian <brianmakorious@gmail.com>
2023-03-06 23:03:45 +0300

    login def

--- task-manage/src/component/Login.jsx
+++ task-manage/src/component/Login.jsx
@@ -1,70 +1,41 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Layout from "../component/Layout";
-import { useRecoilState } from "recoil";
-import { Avatar, Box, Typography } from "@mui/material";
-import AssignmentIcon from "@mui/icons-material/Assignment";
-import { useCookies } from "react-cookie";
-import { tasksState } from "../recoil/atom/taskAtom";
-import AddTask from "../component/AddTask";
-import TaskList from "../component/TaskList";
-import taskApi from "../api/taskApi";
-import { toast } from "react-toastify";
-import CircularProgress from "@mui/material/CircularProgress";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-const Dashboard = () => {
-  const navigate = useNavigate();
-  const [, setTasks] = useRecoilState(tasksState);
-  const [isFetching, setIsFetching] = useState(true);
+const Login = ({ setUser }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
-  const [cookies] = useCookies("token");
+  const history = useHistory();
 
-  useEffect(() => {
-    if (!cookies.token) navigate("/login");
+  const handleLogin = (event) => {
+    event.preventDefault();
 
-    (async () => {
-      try {
-        const result = await taskApi.getTasks(cookies.token);
-        if (result.error) {
-          toast.error(result.error);
-        } else {
-          setTasks(result);
-        }
-        setIsFetching(false);
-      } catch (err) {
-        toast.error(err);
-      }
-    })();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    if (username === 'admin' && password === 'password') {
+      setUser({ username: 'admin' });
+      history.push('/');
+    } else {
+      setError('Invalid username or password.');
+    }
+  };
 
   return (
-    <Layout>
-      {isFetching ? (
-        <Box sx={{ display: "flex", justifyContent: "center", marginTop: 8 }}>
-          <CircularProgress />
-        </Box>
-      ) : (
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <AssignmentIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Tasks
-          </Typography>
-          <AddTask />
-          <TaskList />
-        </Box>
-      )}
-    </Layout>
+    <div className="login-container">
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      {error && <div className="error">{error}</div>}
+    </div>
   );
 };
 
-export default Dashboard;
+export default Login;
